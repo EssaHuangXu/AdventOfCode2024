3efb60367ae17dec103cceec9d6825a40e776c5b
EssaHuangXu <hxEssa@126.com>
2024-12-24 17:19:56 +0800

    Add project files.

---
+++ AdventOfCode2024/HistorianHysteria.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024
+{
+	internal class HistorianHysteria
+	{
+		public static int DistanceWithInTowList( int[] left, int[] right )
+		{
+			var count = left.Length >= right.Length ? right.Length : left.Length;
+			Array.Sort( left );
+			Array.Sort( right );
+			int distance = 0;
+			for( int i = 0; i < count; i++ )
+			{
+				distance += Math.Abs( left[i] - right[i] );
+			}
+
+			return distance;
+		}
+
+		public static int SimilarityWithInTowList( int[] left, int[] right )
+		{
+			var dic = right.GroupBy( x => x ).ToDictionary( g => g.Key, g => g.Count() );
+			var result = 0;
+			for( int i = 0; i < left.Length; i++ )
+			{
+				result += left[i] * ( dic.TryGetValue( left[i], out int value ) ? value : 0 );
+			}
+			return result;
+		}
+	}
+}
---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+namespace AdventOfCode2024
+{
+	internal class Program
+	{
+		static void Main( string[] args )
+		{
+			// Day 1
+			List<int> left = [];
+			List<int> right = [];
+			using FileStream fileStream = new( "HistorianHysteriaInput.txt", FileMode.Open );
+			using StreamReader reader = new( fileStream );
+			var line = reader.ReadLine();
+			while( line != null )
+			{
+				var split = line.Split("   ");
+				if ( split.Length >= 2 )
+				{
+					left.Add( int.Parse( split[0] ) );
+					right.Add( int.Parse( split[1] ) );
+				}
+				line = reader.ReadLine();
+			}
+			var result = HistorianHysteria.DistanceWithInTowList( [.. left], [.. right] );
+			Console.WriteLine( "AdventOfCode2024 Day 1 HistorianHysteria Result is :" + result.ToString() );
+			result = HistorianHysteria.SimilarityWithInTowList( [.. left], [.. right] );
+			Console.WriteLine( "AdventOfCode2024 Day 1 HistorianHysteria Similarity Result is :" + result.ToString() );
+		}
+	}
+}
